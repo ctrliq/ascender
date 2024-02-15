2c58c17bd8e8384af71650106738167bcf61b88e
Jimmy Conner <jconner@ciq.com>
2024-02-14 22:30:51 -0600

    Fix migrations

--- awx/main/migrations/0188_add_bitbucket_dc_webhook.py
+++ awx/main/migrations/0188_add_bitbucket_dc_webhook.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('main', '0187_hop_nodes'),
+        ('main', '0187a_mountain_credential'),
     ]
 
     operations = [
