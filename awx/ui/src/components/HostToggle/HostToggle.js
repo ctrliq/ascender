dddc35d4cb3f78c34772c8d75ccb78d1ccf5d5a7
Zorina Simeonova <zsimeonova@ciq.com>
2025-05-15 11:16:40 -0400

    Migrate JobCancelButton and JobList

--- awx/ui/src/components/HostToggle/HostToggle.js
+++ awx/ui/src/components/HostToggle/HostToggle.js
@@ -14,11 +14,14 @@ function HostToggle({
   host,
   isDisabled = false,
   onToggle,
-  tooltip = i18n._(msg`Indicates if a host is available and should be included in running
-    jobs.  For hosts that are part of an external inventory, this may be
-    reset by the inventory sync process.`),
+  tooltip,
 }) {
   const { i18n } = useLingui();
+  if (!tooltip) {
+    tooltip = i18n._(msg`Indicates if a host is available and should be included in running
+    jobs.  For hosts that are part of an external inventory, this may be
+    reset by the inventory sync process.`);
+  }
   const [isEnabled, setIsEnabled] = useState(host.enabled);
   const [showError, setShowError] = useState(false);
 
