4a0d5f4be1ca89a94ea9760bb4101b4935edc0ae
Jimmy Conner <jconner@ciq.com>
2025-08-01 00:33:55 -0500

    Update requirements.txt for docs

---
+++ docs/docsite/updater.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -ue
+
+venv="`pwd`/venv"
+echo $venv
+/usr/bin/python3.11 -m venv "${venv}"
+# shellcheck disable=SC1090
+source ${venv}/bin/activate
+
+# FIXME: https://github.com/jazzband/pip-tools/issues/1558
+${venv}/bin/python3 -m pip install -U 'pip<22.0' pip-tools
+
+pip-compile --upgrade --no-header --quiet -r --allow-unsafe requirements.in --output-file requirements.txt
+
+rm -fr "${venv}"
+echo "Updated requirements.txt with latest dependencies."
