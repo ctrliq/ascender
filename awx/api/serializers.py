66353056a30e05d9be7b053e8b90a8d612d85218
Jimmy Conner <jconner@ciq.com>
2026-03-29 02:12:01 -0500

    More fixes

--- awx/api/serializers.py
+++ awx/api/serializers.py
@@ -1659,7 +1659,8 @@ class ProjectUpdateCancelSerializer(ProjectUpdateSerializer):
 
 
 class ExecutionEnvironmentBuilderSerializer(BaseSerializer):
-    show_capabilities = ['edit', 'delete', 'copy']
+    show_capabilities = ['start', 'edit', 'delete', 'copy']
+    capabilities_prefetch = ['admin']
 
     class Meta:
         model = ExecutionEnvironmentBuilder
