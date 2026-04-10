43148d9d4cdc311abb8a7e28f8f5846fe74150fb
Jimmy Conner <jconner@ciq.com>
2026-04-10 01:33:45 -0500

    Resolve issue where Elapsed Timer doesn't start on Relaunch

--- awx/ui/src/screens/Job/useWsJob.js
+++ awx/ui/src/screens/Job/useWsJob.js
@@ -20,7 +20,9 @@ export default function useWsJob(initialJob) {
     }
 
     if (
-      ['successful', 'failed', 'error', 'cancelled'].includes(message.status)
+      ['successful', 'failed', 'error', 'cancelled', 'running'].includes(
+        message.status
+      )
     ) {
       fetchJob();
     }
