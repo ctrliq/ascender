cae0887ecd1f80d128c86dbb58019698ecec12eb
Jimmy Conner <jconner@ciq.com>
2026-05-12 08:41:37 -0700

    Make Snyk happy (#337)
    Remove unused service work function
    Add checks to version_chooser (we currently don't use it but may in the future)

--- awx/ui/src/service-worker.js
+++ awx/ui/src/service-worker.js
@@ -81,9 +81,3 @@ self.addEventListener('fetch', (event) => {
       .catch(() => fetch(event.request))
   );
 });
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
