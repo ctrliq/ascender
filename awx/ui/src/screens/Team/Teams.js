6d47dd9a469d70f21d084473e7dcbae1bea812d0
blaipr <blaipr@hotmail.com>
2026-06-14 22:25:22 +0200

    Address review comment: fix the doubled '/* /*' in the nested-route JSX comment

--- awx/ui/src/screens/Team/Teams.js
+++ awx/ui/src/screens/Team/Teams.js
@@ -42,7 +42,7 @@ function Teams() {
       <ScreenHeader streamType="team" breadcrumbConfig={breadcrumbConfig} />
       <Routes>
         <Route path="/teams/add" element={<TeamAdd />} />
-        {/* /* so the nested <Team> route tree can match the rest */}
+        {/* so the nested <Team> route tree can match the rest */}
         <Route
           path="/teams/:id/*"
           element={<Team setBreadcrumb={buildBreadcrumbConfig} />}
