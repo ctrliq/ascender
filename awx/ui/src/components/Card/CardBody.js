1060b49a141712f9cf9f77eea0af534ff74f2754
Jimmy Conner <jconner@ciq.com>
2026-07-01 07:49:18 -0700

    Fix more css issues with darkmode (#556)
    * Fix more css issues with darkmode
    
    * Fix label groups being clipped
    
    * Fix radio colors
    
    * Fix small button heights and bleed over from lightmode
    
    * More fixes
    
    * A few more fixes
    
    * More fixes
    
    * A bit more touchup
    
    * Fix cardbody padding using invalid v6 variable
    
    * A big of cleanup
    
    * Fix pagination drop down to blend in more

--- awx/ui/src/components/Card/CardBody.js
+++ awx/ui/src/components/Card/CardBody.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { CardBody } from '@patternfly/react-core';
 
 const TabbedCardBody = styled(CardBody)`
-  padding-top: var(--pf-v6-c-card--first-child--PaddingTop);
+  padding-block-start: var(--pf-v6-c-card--first-child--PaddingBlockStart);
 `;
 CardBody.displayName = 'PFCardBody';
 
