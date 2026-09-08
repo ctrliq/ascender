503b5d16380ee2e0c69ede2cb4a698b932877ced
Blai Peidro <blaipr@hotmail.com>
2026-09-08 22:46:06 +0200

    fix: move the OAuth2 refresh and ID token models into the main app (#897)

--- awx/main/tests/functional/commands/test_oauth2_token_revoke.py
+++ awx/main/tests/functional/commands/test_oauth2_token_revoke.py
@@ -10,7 +10,7 @@ from django.core.management import call_command
 from django.core.management.base import CommandError
 
 # AWX
-from awx.main.models import RefreshToken
+from awx.main.models import OAuth2RefreshToken
 from awx.main.models.oauth import OAuth2AccessToken
 from awx.api.versioning import reverse
 
@@ -47,16 +47,16 @@ class TestOAuth2RevokeCommand:
         url = reverse('api:o_auth2_token_list')
         post(url, {'description': 'test token for', 'scope': 'read', 'application': oauth_application.pk}, admin)
         assert OAuth2AccessToken.objects.count() == 1
-        assert RefreshToken.objects.count() == 1
+        assert OAuth2RefreshToken.objects.count() == 1
 
         call_command('revoke_oauth2_tokens')
         assert OAuth2AccessToken.objects.count() == 0
-        assert RefreshToken.objects.count() == 1
-        for r in RefreshToken.objects.all():
+        assert OAuth2RefreshToken.objects.count() == 1
+        for r in OAuth2RefreshToken.objects.all():
             assert r.revoked is None
 
         call_command('revoke_oauth2_tokens', '--all')
-        assert RefreshToken.objects.count() == 1
-        for r in RefreshToken.objects.all():
+        assert OAuth2RefreshToken.objects.count() == 1
+        for r in OAuth2RefreshToken.objects.all():
             assert r.revoked is not None
             assert isinstance(r.revoked, datetime.datetime)
