9b678968e1c03cc749ec9923f5b1d04e6663b860
Blai Peidro <blaipr@hotmail.com>
2026-09-13 21:52:41 +0200

    refactor: give every resource its own views and serializers module (#951)

--- awx/api/urls/ad_hoc_command_event.py
+++ awx/api/urls/ad_hoc_command_event.py
@@ -3,7 +3,9 @@
 
 from django.urls import path
 
-from awx.api.views import AdHocCommandEventDetail
+from awx.api.views.ad_hoc_command import (
+    AdHocCommandEventDetail,
+)
 
 urls = [
     path('<int:pk>/', AdHocCommandEventDetail.as_view(), name='ad_hoc_command_event_detail'),
